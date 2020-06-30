aca34cb3c41541bc30a9f4165ee6c132f3fbe56e
Fredrik Trobro <trobro@gmail.com>
2020-06-30 16:06:50 +0200

    trigger error

--- test/test.cpp
+++ test/test.cpp
@@ -1,10 +1,17 @@
+#include <cstdio>
+
+
 void test_value();
 void test_marshal();
 
 
 int main() {
+  char tmp[1];
   test_value();
   test_marshal();
+
+  tmp[1000] = 'a';
+  printf("%s\n", tmp);
 
   return 0;
 }
