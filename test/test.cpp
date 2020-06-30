8a0a6c6ce939e0efe9372782c85fb7ac06926fb5
Fredrik Trobro <trobro@gmail.com>
2020-06-30 16:40:48 +0200

    remove error

--- test/test.cpp
+++ test/test.cpp
@@ -1,17 +1,10 @@
-#include <cstdio>
-
-
 void test_value();
 void test_marshal();
 
 
 int main() {
-  char tmp[1];
   test_value();
   test_marshal();
-
-  tmp[1000] = 'a';
-  printf("%s\n", tmp);
 
   return 0;
 }
